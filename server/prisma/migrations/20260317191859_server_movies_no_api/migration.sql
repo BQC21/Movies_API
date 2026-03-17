5dac67c405271eba6a69681f227d0cb6aa3dbe27
Braulio <braulio.quine@utec.edu.pe>
2026-03-17 14:19:53 -0500

    adjust prisma scheme

---
+++ server/prisma/migrations/20260317191859_server_movies_no_api/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imdb_id` on the `favorite` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "favorite" DROP COLUMN "imdb_id";
